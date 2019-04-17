27dd040c77901ad15ccfdefd47a5726280196e3c
Raj Sekhar Bandhakavi <rsb503@nyu.edu>
2019-04-17 09:56:12 -0400

    modified performance testing

--- performance Testing/Iteratejson.sh
+++ performance Testing/Iteratejson.sh
@@ -1,15 +1,23 @@
 start=`date +%s`
+
 declare -a arr=()
 while IFS= read -r var
 do
   arr+=( $var )
-done < "accounts5k.json"
-args=''
+done < "accounts1k.json"
+accounts=''
+i=0
 for j in "${arr[@]}"
  do
-   args="$args $j"
+   ((i+=1))
+   accounts="$accounts $j"
+   if [ "$i" == 500 ]; then
+     printf -v json '{"Args":["InitLedgerAdvanced","%s"]}' "${accounts//\"/\\\"}"
+     peer chaincode invoke -n cc -C channel1 -c "$json"
+     accounts=''
+     i=0
+   fi
  done
- peer chaincode invoke -n cc -C channel1 -c '{"Args":["InitLedgerAdvanced",'"\"$args\""']}'
 end=`date +%s`
 runtime=$((end-start))
 echo "time taken:"
